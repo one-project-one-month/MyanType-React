26b1bc48be16b9fe7c87586bd7ba3a1e23114412
Ye Thiha Htun <yethihahtun1494@gmail.com>
2025-05-16 17:09:04 +0630

    Context + Axios Added

---
+++ src/api/axiosConfig.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:8181",
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.error("Unauthorized request");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
