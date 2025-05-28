439b07b1b037e030d5a266e44806b804c79f1621
David-Sang96 <luainawl@gmail.com>
2025-05-28 15:23:05 +0800

    feat: add api integration in auth forms

--- src/utils/authFormSchema.js
+++ src/utils/authFormSchema.js
@@ -1,7 +1,9 @@
 import * as z from "zod";
 
 export const registerFormSchema = z.object({
-  name: z.string().min(2, { message: "Name must be at least 2 characters." }),
+  username: z
+    .string()
+    .min(2, { message: "Name must be at least 2 characters." }),
   email: z.string().email({ message: "Invalid email address." }),
   password: z
     .string()
