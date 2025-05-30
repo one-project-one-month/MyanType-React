117b7ad01d88bf0992817606005cb47248ed7806
Ye Thiha Htun <yethihahtun1494@gmail.com>
2025-05-31 00:36:03 +0630

    resolve merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -13,43 +13,32 @@ import Profile from "@/pages/Profile.jsx";
 import Leaderboard from "@/pages/Leaderboard.jsx";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-function App() {
-    return (
 const queryClient = new QueryClient();
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-        <SettingsProvider>
-            <UserStatProvider>
-                <TestProvider>
-                    <Router>
-                        <Routes>
-
-                            <Route path="/" element={<RootLayout/>}>
-                                <Route index element={<HomePage/>}/>
-                                <Route path="sign-up" element={<Register/>}/>
-                                <Route path="login" element={<Login/>}/>
-                                <Route path="results" element={<TypingResults/>}/>
-                            </Route>
-                        </Routes>
-                        <Toaster richColors position="bottom-center"/>
-                            {/*<Route path="/" element={<HomePage />}>*/}
-                            <Route path="sign-up" element={<Register />} />
-                            <Route path="login" element={<Login />} />
-                            <Route path="/challenge" element={<Challenge />} />
-                            <Route path="/results" element={<TypingResults />} />
-                            <Route path='/leaderboard' element={<Leaderboard />} />
-                            <Route path='/profile' element={<Profile />} />
-                            {/*</Route>*/}
-                        </Routes>
-                        <Toaster richColors position="bottom-center" />
-                    </Router>
-                </TestProvider>
-            </UserStatProvider>
-        </SettingsProvider>
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <SettingsProvider>
+                <UserStatProvider>
+                    <TestProvider>
+                        <Router>
+                            <Routes>
+                                <Route path="/" element={<RootLayout/>}>
+                                    <Route index element={<HomePage/>}/>
+                                    <Route path="sign-up" element={<Register/>}/>
+                                    <Route path="login" element={<Login/>}/>
+                                    <Route path="results" element={<TypingResults/>}/>
+                                    <Route path="leaderboard" element={<Leaderboard/>}/>
+                                    <Route path="profile" element={<Profile/>}/>
+                                </Route>
+                            </Routes>
+                            <Toaster richColors position="bottom-center" />
+                        </Router>
+                    </TestProvider>
+                </UserStatProvider>
+            </SettingsProvider>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
