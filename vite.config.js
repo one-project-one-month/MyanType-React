bdf0f38166bc274ded22b5a27461497299c1be1c
Ye Thiha Htun <yethihahtun1494@gmail.com>
2025-05-30 13:42:43 +0630

    Vite Config Changes

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,23 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "build", // Match Render's Publish Directory
+    chunkSizeWarningLimit: 1000, // Increase limit to 1000 kB to reduce warnings
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Example: Split vendor libraries into a separate chunk
+          vendor: ["react", "react-dom"],
+        },
+      },
     },
   },
 });
