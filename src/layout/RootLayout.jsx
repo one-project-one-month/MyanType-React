e4ad9786a238792813d5b4a731a333822f28b0d9
Bhone Wai <demanaosan@gmail.com>
2025-06-01 21:44:32 +0700

    Modify leadreboard and profil

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -3,7 +3,7 @@ import { Toaster } from "sonner";
 
 const RootLayout = () => {
   return (
-    <section className="mx-auto max-w-6xl my-2">
+    <section className="mx-auto max-w-7xl my-2">
       <Toaster
         position="top-center"
         closeButton
